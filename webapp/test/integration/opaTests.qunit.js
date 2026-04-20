d2d611accc3e1566cf7c17890a176e5bbd32bfb2
Quy Le <mr.lpquy@gmail.com>
2026-04-20 09:51:24 +0200

    initial

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["meinetodolistenapp/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"meinetodolistenapp/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
